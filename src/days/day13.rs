8a2a2ba68d8473b07d1848d7ad485307e1d38e71
LennardKittner <lennard@kittner.dev>
2023-12-13 21:28:57 +0100

    day 13

---
+++ src/days/day13.rs
@@ -0,0 +1,117 @@
+use std::usize;
+use itertools::Itertools;
+
+pub fn exec_day13_part1(input: &str) -> String {
+    let mut grids = Vec::new();
+    grids.push(Vec::new());
+    for line in input.lines() {
+        if !line.contains('#') {
+            grids.push(Vec::new());
+            continue;
+        }
+        grids.last_mut().unwrap().push(line.as_bytes());
+    }
+    let mut result = 0;
+    for grid in &grids {
+        let len = grid[0].len();
+        let mut valid = true;
+        for axis in 1..len {
+            valid = true;
+            for line in grid {
+                let dist = usize::min(axis, len - axis);
+                let mut second_half = line[(axis)..(axis + dist)].to_vec();
+                second_half.reverse();
+                if line[(axis - dist)..axis] != second_half {
+                    valid = false;
+                    break;
+                }
+            }
+            if valid {
+                result += axis;
+                break;
+            }
+        }
+        if valid {
+            continue;
+        }
+        for axis in 1..grid.len() {
+            valid = true;
+            for i in 0..usize::min(axis, grid.len() - axis) {
+                if grid[axis -i - 1] != grid[axis + i] {
+                    valid = false;
+                    break;
+                }
+            }
+            if valid {
+                result += axis * 100;
+                break;
+            }
+        }
+    }
+    result.to_string()
+}
+
+
+fn compare(a1: &[u8], a2: &[u8]) -> i32 {
+    if a1.len() != a2.len() {
+        return 400;
+    }
+    let mut diff = 0;
+    for (i, a) in a1.iter().enumerate() {
+        if *a != a2[i] {
+            diff += 1;
+        }
+        if diff > 2 {
+            break;
+        }
+    }
+   diff
+}
+
+pub fn exec_day13_part2(input: &str) -> String {
+    let mut grids = Vec::new();
+    grids.push(Vec::new());
+    for line in input.lines() {
+        if !line.contains('#') {
+            grids.push(Vec::new());
+            continue;
+        }
+        grids.last_mut().unwrap().push(line.as_bytes());
+    }
+    let mut result = 0;
+    for grid in &grids {
+        let len = grid[0].len();
+        let mut valid = true;
+        for axis in 1..len {
+            valid = false;
+            let mut diff = 0;
+            for line in grid {
+                let dist = usize::min(axis, len - axis);
+                let mut second_half = line[(axis)..(axis + dist)].to_vec();
+                second_half.reverse();
+                diff += compare(&line[(axis - dist)..axis], &second_half);
+            }
+            if diff == 1 {
+                valid = true;
+                result += axis;
+                break;
+            }
+        }
+        if valid {
+            continue;
+        }
+        for axis in 1..grid.len() {
+            valid = false;
+            let mut diff = 0;
+            for i in 0..usize::min(axis, grid.len() - axis) {
+                diff += compare(&grid[axis -i - 1], &grid[axis + i]);
+            }
+            if diff == 1 {
+                valid = true;
+                result += axis * 100;
+                break;
+            }
+        }
+    }
+    result.to_string()
+}
--- src/days/mod.rs
+++ src/days/mod.rs
@@ -2,18 +2,19 @@ use std::{fs, i32};
 
 use crate::exec;
 
-mod day9;
-mod day10;
-mod day11;
-mod day8;
 mod day3;
-mod day7;
-mod day6;
-mod day2;
-mod day5;
-mod day1;
+mod day11;
 mod day4;
+mod day1;
+mod day7;
+mod day2;
+mod day8;
 mod day12;
+mod day5;
+mod day10;
+mod day9;
+mod day6;
+mod day13;
 
 
 pub fn run(day: i32) {
@@ -23,53 +24,57 @@ pub fn run(day: i32) {
     };
     match day {
         t if t < 0 => (),
-        9 => {
-            exec(day9::exec_day9_part1, &input);
-            exec(day9::exec_day9_part2, &input);
-        },
-        10 => {
-            exec(day10::exec_day10_part1, &input);
-            exec(day10::exec_day10_part2, &input);
+        3 => {
+            exec(day3::exec_day3_part1, &input);
+            exec(day3::exec_day3_part2, &input);
         },
         11 => {
             exec(day11::exec_day11_part1, &input);
             exec(day11::exec_day11_part2, &input);
         },
-        8 => {
-            exec(day8::exec_day8_part1, &input);
-            exec(day8::exec_day8_part2, &input);
-        },
-        3 => {
-            exec(day3::exec_day3_part1, &input);
-            exec(day3::exec_day3_part2, &input);
-        },
-        7 => {
-            exec(day7::exec_day7_part1, &input);
-            exec(day7::exec_day7_part2, &input);
-        },
-        6 => {
-            exec(day6::exec_day6_part1, &input);
-            exec(day6::exec_day6_part2, &input);
-        },
-        2 => {
-            exec(day2::exec_day2_part1, &input);
-            exec(day2::exec_day2_part2, &input);
-        },
-        5 => {
-            exec(day5::exec_day5_part1, &input);
-            exec(day5::exec_day5_part2, &input);
+        4 => {
+            exec(day4::exec_day4_part1, &input);
+            exec(day4::exec_day4_part2, &input);
         },
         1 => {
             exec(day1::exec_day1_part1, &input);
             exec(day1::exec_day1_part2, &input);
         },
-        4 => {
-            exec(day4::exec_day4_part1, &input);
-            exec(day4::exec_day4_part2, &input);
+        7 => {
+            exec(day7::exec_day7_part1, &input);
+            exec(day7::exec_day7_part2, &input);
+        },
+        2 => {
+            exec(day2::exec_day2_part1, &input);
+            exec(day2::exec_day2_part2, &input);
+        },
+        8 => {
+            exec(day8::exec_day8_part1, &input);
+            exec(day8::exec_day8_part2, &input);
         },
         12 => {
             exec(day12::exec_day12_part1, &input);
             exec(day12::exec_day12_part2, &input);
+        },
+        5 => {
+            exec(day5::exec_day5_part1, &input);
+            exec(day5::exec_day5_part2, &input);
+        },
+        10 => {
+            exec(day10::exec_day10_part1, &input);
+            exec(day10::exec_day10_part2, &input);
+        },
+        9 => {
+            exec(day9::exec_day9_part1, &input);
+            exec(day9::exec_day9_part2, &input);
+        },
+        6 => {
+            exec(day6::exec_day6_part1, &input);
+            exec(day6::exec_day6_part2, &input);
+        },
+        13 => {
+            exec(day13::exec_day13_part1, &input);
+            exec(day13::exec_day13_part2, &input);
         },
         _ => (),
     }
