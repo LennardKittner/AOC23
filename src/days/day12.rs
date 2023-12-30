b625b9f82fd47cbda3a98dcf6db7980d6788ab9d
Lennard Kittner <lennardkittner@icloud.com>
2023-12-30 01:42:27 +0100

    day 12

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,19 +1,23 @@
+use std::collections::HashMap;
 use itertools::Itertools;
 
-fn combinations(springs: &[u8], nums: &[usize]) -> i32 {
+fn combinations(cache: &mut HashMap<(usize, usize), u64>, springs: &[u8], nums: &[usize]) -> u64 {
     if nums.is_empty() {
         return if springs.contains(&b'#') { 0 } else { 1 };
     } else if springs.is_empty() || nums[0] > springs.len() {
         return 0;
     }
+    if let Some(res) = cache.get(&(springs.len(), nums.len())) {
+        return *res;
+    }
     let mut springs = springs.to_vec();
-    match springs[0] {
+    let result = match springs[0] {
         b'.' => {
-            combinations(&springs[1..], nums)
+            combinations(cache, &springs[1..], nums)
         },
         b'#' => {
             if !springs[..nums[0]].contains(&b'.') && (springs.len() == nums[0] || springs[nums[0]] != b'#') {
-                combinations(if springs.len() == nums[0] { &[] } else { &springs[(nums[0]+1)..] }, &nums[1..])
+                combinations(cache, if springs.len() == nums[0] { &[] } else { &springs[(nums[0]+1)..] }, &nums[1..])
             } else {
                 0
             }
@@ -22,34 +26,47 @@ fn combinations(springs: &[u8], nums: &[usize]) -> i32 {
             springs[0] = b'#';
             let mut result = 0;
             if !springs[..nums[0]].contains(&b'.') && (springs.len() == nums[0] || springs[nums[0]] != b'#') {
-                result += combinations(if springs.len() == nums[0] { &[] } else { &springs[(nums[0]+1)..] }, &nums[1..])
+                result += combinations(cache, if springs.len() == nums[0] { &[] } else { &springs[(nums[0]+1)..] }, &nums[1..])
             }
             springs[0] = b'.';
-            result += combinations(if springs.len() == 1 { &[] } else { &springs[1..] }, nums);
+            result += combinations(cache, if springs.len() == 1 { &[] } else { &springs[1..] }, nums);
             result
         },
         _ => panic!("unknons symbol")
-    }
+    };
+    cache.insert((springs.len(), nums.len()), result);
+    result
 }
-
 
 pub fn exec_day12_part1(input: &str) -> String {
     let lines = input.lines()
     .map(|l| l.split(' ').collect_vec())
     .fold(Vec::new(), |mut a, l| {
-        let springs = l[0].as_bytes();
+        let springs = l[0].as_bytes().to_vec();
         let nums = l[1].split(',').map(|n| n.parse::<usize>().unwrap()).collect_vec();
         a.push((springs, nums));
         a
     });
+    day12(lines).to_string()
+}
+
+fn day12(lines: Vec<(Vec<u8>, Vec<usize>)>) -> u64 {
     let mut result = 0;
-    for line in lines {
-        let tmp = combinations(line.0, &line.1);
-        result += tmp;
+    for line in lines.iter() {
+        let mut cache = HashMap::new();
+        result += combinations(&mut cache, &line.0, &line.1);
     }
-    result.to_string()
+    result
 }
 
 pub fn exec_day12_part2(input: &str) -> String {
-    todo!("{}", input.len())
+    let lines = input.lines()
+        .map(|l| l.split(' ').collect_vec())
+        .fold(Vec::new(), |mut a, l| {
+            let springs = format!("{0}?{0}?{0}?{0}?{0}", l[0]).as_bytes().to_vec();
+            let nums = format!("{0},{0},{0},{0},{0}", l[1]).split(',').map(|n| n.parse::<usize>().unwrap()).collect_vec();
+            a.push((springs, nums));
+            a
+        });
+    day12(lines).to_string()
 }
